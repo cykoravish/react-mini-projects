8e7cacd75118e0a50d47f8c0a3383fc287c92909
Ravish Bisht <ravishbisht85@gmail.com>
2024-02-16 09:05:42 +0530

    ui complete

--- notes/src/App.jsx
+++ notes/src/App.jsx
@@ -1,9 +1,49 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTods] = useState([]);
+
+  const handleEdit = () => {};
+  const handleDelete = () => {};
+  const handleAdd = () => {};
 
   return (
     <>
-      <h1 className="bg-red-600">hi</h1>
+      <Navbar />
+      <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh]">
+        <div className="addTodo my-5">
+          <h2 className="text-lg font-bold">Add a Todo</h2>
+          <input type="text" className="w-1/2" />
+          <button
+            onclick={handleAdd}
+            className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-6"
+          >
+            Add
+          </button>
+        </div>
+        <h2 className="text-xl font-bold">Your Todos</h2>
+        <div className="todos">
+          <div className="todo flex">
+            <div className="text">{todo}</div>
+            <div className="buttons">
+              <button
+                onClick={handleEdit}
+                className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1"
+              >
+                Edit
+              </button>
+              <button
+                onClick={handleDelete}
+                className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1"
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
